c0c61d008035c4b7aafde923aa41cb9deb4607f8
kriistenn <christinadefault@gmail.com>
2020-04-27 14:52:40 +0300

    removeHalfOfTheCode

--- script.js
+++ script.js
@@ -1,42 +1,30 @@
+const list = document.querySelector('#list');
+
+const renderTask = (task) => {
+    const li = document.createElement('li')
+
+    li.textContent = task.textContent
+    li.setAttribute('data-numbers', task.id)
+
+    if(task.done) li.classList.add('done')
+    list.appendChild(li)
+}
 fetch('http://localhost:3000/list')
-.then(response => response.json())
-.then(data => {
-    const list = document.querySelector('#list');
-
-    data.forEach(item => {
-        const li = document.createElement('li');
-        li.textContent = item.text;
-        li.setAttribute('data-number', item.id)
-
-        if(item.done) li.classList.add('done')
-
-        list.appendChild(li)
-    })
+.then(response => {
+    return response.json()
 })
-.then(() => {
-    const todoItems = document.querySelectorAll('li')
-
-    todoItems.forEach(li => {
-        li.addEventListener('click', () => {
-            const desc = document.createElement('div')
-            desc.className = 'desc'
-            const id = li.getAttribute('data-number')
-
-            if(li.querySelector('.desc')) return
-            
-
-        fetch(`http://localhost:3000/list/${id}`)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data)
-                    desc.textContent = data.description;
-                    desc.style.backgroundColor = data.color;
-                    
-                    li.appendChild(desc);
-                    return desc;
-
-            })
-        })
-    })
+.then(taskList => {
+    taskList.forEach(renderTask)
 })
+
+
+
+
+
+
+
+
+
+
+
 
